f20fdad12dcbd8e71ee579f5c3bce3e869e16a17
IsaMontoya17 <isabelamontoya2021@gmail.com>
2025-09-11 11:41:51 -0500

    fix: make footer stick to bottom using flex layout

--- src/components/layout/Layout.jsx
+++ src/components/layout/Layout.jsx
@@ -1,17 +1,18 @@
 import { Outlet } from 'react-router-dom'
 import Header from '../header/Header'
 import Footer from '../footer/Footer'
+import styles from './Layout.module.css'
 
 function Layout() {
   return (
-    <div className="app">
+    <div className={styles.layout}>
       <Header />
-      <main>
-        <Outlet /> 
+      <main className={styles.main}>
+        <Outlet />
       </main>
       <Footer />
     </div>
-  )
+  );
 }
 
 export default Layout
