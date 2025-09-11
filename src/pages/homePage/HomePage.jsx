ca8499314afbec8aefb85dee12e44b4bdd4972b5
IsaMontoya17 <isabelamontoya2021@gmail.com>
2025-09-11 11:31:05 -0500

    feat: add main pages base and navbar implementation

---
+++ src/pages/homePage/HomePage.jsx
@@ -0,0 +1,10 @@
+const HomePage = () => {
+  return (
+    <div>
+      <h1>Home Page</h1>
+      <p>Welcome to EcoCloset, your sustainable fashion hub.</p>
+    </div>
+  )
+}
+
+export default HomePage
