31447ec89c8987aea90d1ae5722e8b8faaf8ad9e
IsaMontoya17 <isabelamontoya2021@gmail.com>
2025-09-12 10:46:17 -0500

    feat: initialize directory component

---
+++ src/pages/user/directory/Directory.jsx
@@ -0,0 +1,10 @@
+const Directory = () => {
+  return (
+    <div>
+      <h1>Directorio de Tiendas</h1>
+      <p>Aquí encontrarás una lista de tiendas de segunda mano y moda sostenible.</p>
+    </div>
+  )
+}
+
+export default Directory
