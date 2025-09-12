5245744e3ce68140edb2c0df9b60ba9d4017e8e7
IsaMontoya17 <isabelamontoya2021@gmail.com>
2025-09-12 07:29:57 -0500

    chore: separate layouts for public and admin pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,9 @@
 import './App.css'
 import { Routes, Route } from 'react-router-dom';
 import HomePage from './pages/homePage/HomePage';
-import NotFound from './pages/notFound/NotFound'; 
+import NotFound from './pages/notFound/NotFound';
 import Layout from './components/layout/Layout';
+import AdminPage from './pages/admin/adminPage/AdminPage';
 
 function App() {
 
@@ -13,8 +14,11 @@ function App() {
           <Route index element={<HomePage />} />
           <Route path="*" element={<NotFound />} />
         </Route>
+        
+        <Route path="/admin" element={<AdminPage />} />
       </Routes>
-      
+
+
     </>
   )
 }
